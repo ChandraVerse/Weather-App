23c95e1243a33c49a47831dba61e8ceb91fde967
Chandra Sekhar Chakraborty <chakrabortychandrasekhar@gmail.com>
2025-06-26 20:22:10 +0530

    Final

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), 
+  plugins: [react(),  
     tailwindcss(),
   ],
-})
+  base: '/Weather-App/',
+});
